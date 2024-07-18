1031aa7906e08482142b849e5a2c54021d0bb218
Akachukwu <okoli555aka@gmail.com>
2024-07-18 01:46:30 +0100

    adjusted empty classnames in some image tags

--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -113,7 +113,7 @@ export default function Favorites() {
             (
               <div className='w-full flex flex-col items-center mt-[20px] mb-[80px]'>
                 <div className='relative w-[100px] h-[100px]'>
-                  <Image alt='illustration of favorite list' fill className='' src='/favorite_items.svg'/>
+                  <Image alt='illustration of favorite list' fill src='/favorite_items.svg'/>
                 </div>
                 <h2 className={`${poppins.className} font-[600] my-[10px] text-[14px]`}>Your favourite list is empty</h2>
                 <h2 className={`${poppins.className} font-[400] text-[12px] max-w-[350px] text-center`}>Looks like you haven't added anything to your favorite list. Go ahead and explore our fascinating products.</h2>
